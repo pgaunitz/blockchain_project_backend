5b08d3a51d6ea35236492193a14946192ef35e2a
Philip Gaunitz <fluffen@pgaunitz.se>
2020-10-01 23:04:01 +0200

    adds test for genesis and, test goes green

--- block.test.js
+++ block.test.js
@@ -1,10 +1,10 @@
 const Block = require('./block');
-
-describe("Block", () => {
-  const timestamp = "a-date";
-  const lastHash = "last-hash";
-  const hash = "hash";
-  const data = ["blockchain", "data"];
+const { GENESIS_DATA } = require('./config');
+describe('Block', () => {
+  const timestamp = 'a-date';
+  const lastHash = 'last-hash';
+  const hash = 'hash';
+  const data = ['blockchain', 'data'];
   const block = new Block({
     timestamp,
     lastHash,
@@ -12,10 +12,22 @@ describe("Block", () => {
     data,
   });
 
-  it("has a timestamp, lastHash, hash, and data property", () => {
+  it('has a timestamp, lastHash, hash, and data property', () => {
     expect(block.timestamp).toEqual(timestamp);
     expect(block.lastHash).toEqual(lastHash);
     expect(block.hash).toEqual(hash);
     expect(block.data).toEqual(data);
+  });
+
+  describe('genesis', () => {
+    const genesisBlock = Block.genesis();
+
+    it('returns a Block instance', () => {
+      expect(genesisBlock instanceof Block).toBe(true);
+    });
+
+    it('returns the genesis data', () => {
+      expect(genesisBlock).toEqual(GENESIS_DATA);
+    });
   });
 });
