f373333ec9dc867cd7e56fafb04677a352b461f4
Philip Gaunitz <fluffen@pgaunitz.se>
2020-10-19 13:35:43 +0200

    install body-parser,  adds get action

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ app.get('/api/blocks', (req, res) => {
   res.json(blockchain.chain);
 });
 const PORT = 3000;
+
+app.post('api/mine', (req, res) => {
+  const { data } = req.body;
+
+  blockchain.addBlock({ data });
+
+  res.redirect('api/blocks');
+});
+
 app.listen(PORT, () => {
   console.log(`listening at localhost:${PORT}`);
 });
