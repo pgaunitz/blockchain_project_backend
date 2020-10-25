0741327e969a5e3eaf90549de2d155ccff03f96a
Philip Gaunitz <fluffen@pgaunitz.se>
2020-10-25 12:34:54 +0100

    installs elliptic, sets jest testenv to node, adds ec in util

--- wallet/index.js
+++ wallet/index.js
@@ -1,8 +1,12 @@
 const { STRATING_BALANCE } = require('../config');
+const { ec } = require('../util');
 
 class Wallet {
   constructor() {
     this.balance = STRATING_BALANCE;
+
+    const keyPair = ec.genKeyPair();
+    this.publicKey = keyPair.getPublic().encode('hex');
   }
 }
 
