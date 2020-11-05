1faefe3a3fd6d9f5b23fb3f884e0bb50d9c702e7
Philip Gaunitz <fluffen@pgaunitz.se>
2020-11-05 15:44:34 +0100

    adds outputmap and helper function

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -3,6 +3,15 @@ const { v1: uuid } = require('uuid');
 class Transaction {
   constructor({ senderWallet, recipient, amount }) {
     this.id = uuid();
+    this.outputMap = this.createOutputMap({ senderWallet, recipient, amount });
+  }
+
+  createOutputMap({ senderWallet, recipient, amount }) {
+    const outputMap = {};
+    outputMap[recipient] = amount;
+    outputMap[senderWallet.publicKey] = senderWallet.balance - amount;
+
+    return outputMap;
   }
 }
 
