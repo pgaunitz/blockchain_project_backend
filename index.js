8441a7cb80ee21575ae43b380aadf307238d57e1
Philip Gaunitz <fluffen@pgaunitz.se>
2020-11-17 11:53:10 +0100

    adds new post endpoint for making a transaction

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const express = require('express');
 const request = require('request');
 const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
 
 const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
 
 const DEFUALT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFUALT_PORT}`;
@@ -26,6 +30,18 @@ app.post('/api/mine', (req, res) => {
   pubsub.broadcastChain();
 
   res.redirect('/api/blocks');
+});
+
+app.post('/api/transact', (req, res) => {
+  const { amount, recipient } = req.body;
+
+  const transaction = wallet.createTransaction({ recipient, amount });
+
+  transactionPool.setTransaction(transaction);
+
+  console.log('transactionPool', transactionPool);
+
+  res.json({ transaction });
 });
 
 const syncChains = () => {
