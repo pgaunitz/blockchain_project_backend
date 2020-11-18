3995ebe9c4df5f228c225d2135776da87ab1cff2
Philip Gaunitz <fluffen@pgaunitz.se>
2020-11-18 17:05:43 +0100

    adds clearBlockTransaction method tests green

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -26,6 +26,22 @@ class TransactionPool {
       Transaction.validTransaction(transaction)
     );
   }
+
+  clear() {
+    this.transactionMap = {};
+  }
+
+  clearBlockchainTransactions({ chain }) {
+    for (let i = 1; i < chain.length; i++) {
+      const block = chain[i];
+
+      for (const transaction of block.data) {
+        if (this.transactionMap[transaction.id]) {
+          delete this.transactionMap[transaction.id];
+        }
+      }
+    }
+  }
 }
 
 module.exports = TransactionPool;
