1cda9c9283a38dfd0c3630128f635ecd1c98f0be
Philip Gaunitz <fluffen@pgaunitz.se>
2020-11-30 16:35:51 +0100

    completed transaction-miner and adds GET endpoint

--- app/transaction-miner.js
+++ app/transaction-miner.js
@@ -1,3 +1,5 @@
+const Transaction = require('../wallet/transaction');
+
 class TransactionMiner {
   constructor({ blockchain, transactionPool, wallet, pubsub }) {
     this.blockchain = blockchain;
@@ -7,15 +9,17 @@ class TransactionMiner {
   }
 
   mineTransactions() {
-    // get the valid transactions from transaction pool
-    //
-    // generate the miner's reward
-    //
-    // add a block consisting of these transactions to the blockchain
-    //
-    // broadcast the updated blockchain
-    //
-    // clear the pool
+    const validTransactions = this.transactionPool.validTransactions();
+
+    validTransactions.push(
+      Transaction.rewardTransaction({ minerWallet: this.wallet })
+    );
+
+    this.blockchain.addBlock({ data: validTransactions });
+
+    this.pubsub.broadcastChain();
+
+    this.transactionPool.clear();
   }
 }
 
